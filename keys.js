3385b84a9d471a2f2714ce48548fd5cb082e9a76
Nadire Ghalip <nadireghalip@Nadires-MacBook-Pro.local>
2019-03-19 23:26:51 -0700

    changes

--- keys.js
+++ keys.js
@@ -8,3 +8,8 @@ exports.spotify = {
 
 var keys = require("./keys.js");
 var spotify = new Spotify(keys.spotify);
+
+var spotifyKeys = {
+  id: "5f027c20f59b4045b43efba0d3c118ea",
+  secret: "bb60f0c0a52944498a897314e58e1ba4"
+}
