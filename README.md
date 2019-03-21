b53b2607e8eaa0bb0ecd4fafba91015519f29cf8
Nadire Ghalip <nadireghalip@Nadires-MacBook-Pro.local>
2019-03-20 22:58:45 -0700

    added a readMe

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # liri-node-app
+
+#### LIRI is A Node App via Command Line LIRI is like iPhone's SIRI. However, while SIRI is a Speech Interpretation and Recognition Interface, LIRI is a Language Interpretation and Recognition Interface. LIRI will be a command line node app that takes in parameters and gives you back data.
+
+#### Use Liri to look up your favorite artist, song, or movie and get the information you need.
+
+## Technologies Used
+* Node.js
+* JavaScript
+* BansinTown API (via bandsintown npm module)
+* Spotify API (via spotify npm module)
+* OMDb API (via request npm module)
+
+
+
+### Commands to run LIRI
+* node liri.js concert-this < artist name here >
+* node liri.js spotify-this-song < song name here>
+* node liri.js movie-this < movie name here>
+* node liri.js do-what-it-says
+
+## API Credential sites
+* Spotify: https://developer.spotify.com/my-applications/
+* Bandsintown: https://rest.bandsintown.com/artists/
+* OMDb API: http://www.omdbapi.com/apikey.aspx
--- liri.js
+++ liri.js
@@ -18,7 +18,7 @@ var workOfArt = process.argv.slice(3).join(" ");
 
 function whatToDo() {
     if (command === 'do-what-it-says') {
-        fs.readFile('./random.txt', 'UTF8', function(err, data) {
+        fs.readFile('./random.txt', 'UTF8', function (err, data) {
             if (err) {
                 console.log("I don't know what it says!")
             }
@@ -26,21 +26,13 @@ function whatToDo() {
             workOfArt = data.substring(data.indexOf(",") + 2, data.length - 1)
             whatToDo();
         })
-    }
-    
-    else if (command === 'concert-this') {
+    } else if (command === 'concert-this') {
         ConcertThis();
-    }
-    
-    else if (command === 'spotify-this-song') {
+    } else if (command === 'spotify-this-song') {
         SpotifyThis()
-    }
-    
-    else if (command === 'movie-this') {
+    } else if (command === 'movie-this') {
         MovieThis();
-    }
-    
-    else {
+    } else {
         console.log("Enter a valid command")
     }
 }
@@ -52,31 +44,29 @@ function whatToDo() {
 function ConcertThis() {
     if (workOfArt == "") {
         console.log("You must include an artist to search.")
-    }
-    else {
+    } else {
         axios.get("https://rest.bandsintown.com/artists/" + workOfArt + "/events?app_id=codingbootcamp")
-        .then(function(response) {
-            var results = response.data;
-            for (i=0;i<results.length;i++) {
-                var venue = results[i].venue.name;
-                if (results[i].country === "United States") {
-                    var location = results[i].venue.city + ", " + results[i].venue.region
-                }
-                else {
-                    var location = results[i].venue.city + ", " + results[i].venue.country
-                }
-                var date = moment(results[i].datetime)
-                date = date.format("MM/DD/YYYY")
-                var output = ("\nVenue: " + venue + "\nLocation: " + location + "\nDate: " + date + "\n---------------------------------");
-                console.log(output)
-                fs.appendFile('log.txt', output, 'utf8', function(error) {
-                    if (error) {
-                        console.log("Oops! Couldn't write.")
+            .then(function (response) {
+                var results = response.data;
+                for (i = 0; i < results.length; i++) {
+                    var venue = results[i].venue.name;
+                    if (results[i].country === "United States") {
+                        var location = results[i].venue.city + ", " + results[i].venue.region
+                    } else {
+                        var location = results[i].venue.city + ", " + results[i].venue.country
                     }
-                    console.log("Yay! Appended data to file.")
-                })
-            }
-        })
+                    var date = moment(results[i].datetime)
+                    date = date.format("MM/DD/YYYY")
+                    var output = ("\nVenue: " + venue + "\nLocation: " + location + "\nDate: " + date + "\n---------------------------------");
+                    console.log(output)
+                    fs.appendFile('log.txt', output, 'utf8', function (error) {
+                        if (error) {
+                            console.log("Oops! Couldn't write.")
+                        }
+                        console.log("Yay! Appended data to file.")
+                    })
+                }
+            })
     }
 
 }
@@ -90,7 +80,7 @@ function SpotifyThis() {
     spotify.search({
         type: 'track',
         query: workOfArt
-    }, function(err, data) {
+    }, function (err, data) {
         if (err) {
             console.log("Error occurred finding your song")
         }
@@ -101,7 +91,7 @@ function SpotifyThis() {
         var album = results.album.name;
         var output = ("\nArtist: " + artist + "\nSong Name: " + name + "\nPreview Link: " + preview + "\nAlbum: " + album + "\n---------------------------------");
         console.log(output)
-        fs.appendFile('log.txt', output, 'utf8', function(error) {
+        fs.appendFile('log.txt', output, 'utf8', function (error) {
             if (error) {
                 console.log("Oops! Couldn't write.")
             }
@@ -117,37 +107,37 @@ function MovieThis() {
         workOfArt = "Mr. Nobody"
     }
     axios.get("http://www.omdbapi.com/?apikey=trilogy&t=" + workOfArt)
-    .then(function(response) {
-        console.log(response.data.Title)
-        results = response.data;
-        var title = results.Title;
-        var year = results.Year;
-        ratingsArr = results.Ratings
-        var IMDB = ratingsArr.filter(function(item) {
-            return item.Source === 'Internet Movie Database'
-        }).map(function(item) {
-            return item.Value.toString()
+        .then(function (response) {
+            console.log(response.data.Title)
+            results = response.data;
+            var title = results.Title;
+            var year = results.Year;
+            ratingsArr = results.Ratings
+            var IMDB = ratingsArr.filter(function (item) {
+                return item.Source === 'Internet Movie Database'
+            }).map(function (item) {
+                return item.Value.toString()
+            })
+            IMDB = IMDB.toString();
+            var RT = ratingsArr.filter(function (item) {
+                return item.Source === 'Rotten Tomatoes'
+            }).map(function (item) {
+                return item.Value.toString()
+            })
+            RT = RT.toString();
+            country = results.Country;
+            language = results.Language;
+            plot = results.Plot;
+            actors = results.Actors;
+            var output = ("\nTitle: " + title + "\nYear: " + year + "\nIMDB Rating: " + IMDB + "\nRotten Tomatoes Rating: " + RT + "\nCountry: " + country + "\nLanguage: " + language + "\nPlot: " + plot + "\nActors: " + actors + "\n---------------------------------")
+            console.log(output)
+            fs.appendFile('log.txt', output, 'utf8', function (error) {
+                if (error) {
+                    console.log("Oops! Couldn't write.")
+                }
+                console.log("Yay! Appended data to file.")
+            })
         })
-        IMDB = IMDB.toString();
-        var RT = ratingsArr.filter(function(item) {
-            return item.Source === 'Rotten Tomatoes'
-        }).map(function(item) {
-            return item.Value.toString()
-        })
-        RT = RT.toString();
-        country = results.Country;
-        language = results.Language;
-        plot = results.Plot;
-        actors = results.Actors;
-        var output = ("\nTitle: " + title + "\nYear: " + year + "\nIMDB Rating: " + IMDB + "\nRotten Tomatoes Rating: " + RT + "\nCountry: " + country + "\nLanguage: " + language + "\nPlot: " + plot + "\nActors: " + actors + "\n---------------------------------")
-        console.log(output)
-        fs.appendFile('log.txt', output, 'utf8', function(error) {
-            if (error) {
-                console.log("Oops! Couldn't write.")
-            }
-            console.log("Yay! Appended data to file.")
-        })
-    })
 }
 
 whatToDo();
